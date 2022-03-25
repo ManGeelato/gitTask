63d6240010df0c79d477b683af87d455d84c0f35
ManGeelato <92035453+ManGeelato@users.noreply.github.com>
2022-03-25 16:33:09 +0200

    program showing output on the screen

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,11 @@
-// program that prints out Hello World
-alert('Git is Awesome!');
-console.log('Git is Awesome!');
+// Once you are on the issue-1 branch, change the helloWorld.js file. Modify
+// your program to accept input from the user and then print out whatever
+// was input.
+
+//To output the details entered by the user, we need a page so I created HTML file called helloWorld.html and 
+//linked it to this javascript file
+function getInputUser(){
+    var course = window.prompt('Enter course you are doing?: ');
+    alert('Course registered is: '+ course);
+    console.log('Course registered is: '+ course);
+}
